5d985142ce0629d8cfb46d7edbc004c4a0c89ffc
Slava Bezgachev <bezgachev@gmail.com>
2021-08-21 08:49:11 +0600

    Fixed basics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <title>Фыыы</title>
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
