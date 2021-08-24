5251a3b950063f75bfd0f5d1d2477293ebdf77e3
Slava Bezgachev <bezgachev@gmail.com>
2021-08-24 23:06:40 +0600

    Let's try the cron job pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: "shop-watch",
+      name: "watcher",
       script: "yarn run build && yarn run start",
+    },
+    {
+      name: "prices",
+      script: "yarn run update_prices",
+      cron_restart: "0 * * * *",
+      instances: 1,
     },
   ],
 };
